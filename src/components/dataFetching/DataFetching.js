ee1cd3faa6fb186490e7d747ab65b7d5b51e1012
jjadeseravla <jadealvares@hotmail.co.uk>
2020-01-27 14:30:00 +0000

    fetching data on button click rather than onChange effect

--- src/components/dataFetching/DataFetching.js
+++ src/components/dataFetching/DataFetching.js
@@ -2,26 +2,33 @@ import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 
 function DataFetching() {
-    const [posts, setPosts] = useState([]) //initialise posts with []
+    const [post, setPost] = useState([]) //initialise posts with []
+    const [id, setId] = useState(1)
+    const [idFromButtonClick, setIdFromButtonClick] = useState(1)
+
+    const handleClick = () => {
+      setIdFromButtonClick(id)
+    }
 
     useEffect(() => {
-      axios.get('https://jsonplaceholder.typicode.com/posts') //get request returns a promise
+      axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`) //get request returns a promise
         .then(res => {
           console.log(res);
-          setPosts(res.data) //update the posts state variable and render in the UI
+          setPost(res.data) //update the posts state variable and render in the UI
         })
         .catch(err => {
           console.log(err);
         })
-    }, [])
+    }, [idFromButtonClick]) //we depend on the idFromButtonClick for it to change
+    //we can get a single post by passing in the id chosen by the user
+
+//state variable that will only change on a button click
 
     return (
       <div>
-        <ul>
-        {
-          posts.map(post => <li key={post.id}>{post.title}</li>)
-        }
-        </ul>
+      <input type="text" value={id} onChange={e => setId(e.target.value)} />
+        <button type="button" onClick={handleClick}> Fetch Post</button>
+        <div>{post.title}</div>
       </div>
     )
   }
